eaacc3ef61404b2785e4eca447e16fc2b891a2a0
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-02-28 13:55:00 +0100

    warunki

---
+++ lab2/warunki/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x=0;
+    if (x){
+        printf("Prawda\n");
+    }
+    else{
+        printf("falsz\n");
+    }
+    return 0;
+}
